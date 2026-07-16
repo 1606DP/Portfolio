8db212e0236f4eeda8019071f8efe48005428d25
Deepika Pal <117921277+1606DP@users.noreply.github.com>
2026-07-16 22:19:34 +0530

    Refactor MongoDB connection handling
    Updated connection URL environment variable and improved logging.

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,14 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.mongo_url);
+mongoose.connect(process.env.MONGO_URL);
 
-const connection = mongoose.connection;
-connection.on('error', () => {
-  console.log("Error connecting to database");
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB Connected");
 });
 
-connection.on('connected',()=>{
-  console.log('Mongo DB Connection Successfull');
+mongoose.connection.on("error", (err) => {
+  console.log(err);
 });
-
-module.exports = connection;
