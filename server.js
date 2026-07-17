4871cb3c71ea264edc56d4e709db5b134c5283ea
Deepika Pal <117921277+1606DP@users.noreply.github.com>
2026-07-17 11:32:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,16 +7,12 @@ const portfolioRoute = require("./routes/portfolioRoute");
 
 app.use(express.json());
 
-app.use("/api/portfolio", portfolioRoute);
-const cors = require("cors");
+app.use(cors());
 
-app.use(cors({
-  origin: "https://portfolio-1-l56w.onrender.com",
-  credentials: true
-}));
+app.use("/api/portfolio", portfolioRoute);
 
 const port = process.env.PORT || 5000;
 
- app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running on port ${port}`);
- });
+});
