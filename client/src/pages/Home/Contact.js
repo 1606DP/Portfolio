accf01731d4766d30e6a02f1c8a4bdaf2ac05425
Deepika Pal <117921277+1606DP@users.noreply.github.com>
2026-07-17 13:30:24 +0530

    Update Contact.js

--- client/src/pages/Home/Contact.js
+++ client/src/pages/Home/Contact.js
@@ -5,22 +5,20 @@ import { useSelector } from "react-redux";
 
 function Contact() {
   const { portfolioData } = useSelector((state) => state.root);
-  const contact = portfolioData?.contact || {};
-
+  const { contact } = portfolioData;
+  
   return (
     <div>
       <SectionTitle title="Say Hello" />
       <div className="flex sm:flex-col items-center justify-between">
         <div className="flex flex-col ">
           <p className="text-tertiary text-sm">{"{"}</p>
-          {Object.keys(contact).map((key) =>
-            key !== '_id' ? (
-              <p key={key} className="ml-5">
-                <span className="text-tertiary">{key} : </span>
-                <span className="text-tertiary">{contact[key]}</span>
-              </p>
-            ) : null
-          )}
+          {Object.keys(contact).map((key) => (
+            key !=='_id' &&   <p key={key} className="ml-5">
+              <span className="text-tertiary">{key} : </span>
+              <span className="text-tertiary">{contact[key]}</span>
+            </p>
+          ))}
           <p className="text-tertiary">{"}"}</p>
         </div>
         <div className="flex h-[400px] ">
