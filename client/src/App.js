96f5829a5403acbbf1e4665aae59bfebe3a29c8b
Deepika Pal <117921277+1606DP@users.noreply.github.com>
2026-07-17 12:38:11 +0530

    Refactor App component and routing structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,51 @@
-import { message } from "antd";
-import React from "react";
-import { HideLoading, ShowLoading } from "../../redux/rootSlice";
-import api from "../../api";
-import { useDispatch } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Loader from "./components/Loader";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { HideLoading, ReloadData, SetPortfolioData, ShowLoading } from "./redux/rootSlice";
+import Admin from "./pages/Admin";
+import Login from "./pages/Admin/Login";
 
-function Login() {
-  const [user, setUser] = React.useState({
-    username: "",
-    password: "",
-  });
-  const dispatch = useDispatch();
-  const login = async () => {
+function App() {
+  const {loading, portfolioData,reloadData } = useSelector(state => state.root); 
+
+const dispatch = useDispatch();
+  const getPortfolioData = async () => {
     try {
       dispatch(ShowLoading());
-      const response = await api.post("/api/portfolio/admin-login", user);
+      const response = await axios.get("/api/portfolio/get-portfolio-data");
+      dispatch(SetPortfolioData(response.data));
+      dispatch(ReloadData(false));
       dispatch(HideLoading());
-      if (response.data.success) {
-        message.success(response.data.message);
-        localStorage.setItem("token", JSON.stringify(response.data));
-        window.location.href = "/admin";
-      } else {
-        message.error(response.data.message);
-      }
     } catch (error) {
-      message.error(error.message);
       dispatch(HideLoading());
     }
   };
 
+  useEffect(() => {
+    if (!portfolioData) {
+      getPortfolioData();
+    }
+  }, [portfolioData]);
+
+  useEffect(()=> {
+    if(reloadData){
+      getPortfolioData();
+    }
+  }, [reloadData]);
+
   return (
-    <div className="flex justify-center items-center h-screen bg-primary">
-      <div className="w-96 flex gap-5 p-5 shadow border border-gray-500 flex-col bg-white">
-        <h1 className="text-2xl">Portfolio - Admin Login</h1>
-        <hr />
-        <input
-          type="text"
-          value={user.username}
-          onChange={(e) => setUser({ ...user, username: e.target.value })}
-          placeholder="Username"
-        />
-        <input
-          type="password"
-          value={user.password}
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-          placeholder="Password"
-        />
-        <button className="bg-primary text-white p-2" onClick={login}>
-          Login
-        </button>
-      </div>
-    </div>
+    <BrowserRouter>
+      {loading ? <Loader /> : null}
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/admin" element={<Admin />}/>
+        <Route path="/admin-login" element={<Login />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default Login;
+export default App;
