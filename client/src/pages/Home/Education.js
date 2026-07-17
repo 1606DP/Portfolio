fe16cf9db33851f9fea273e19312a4c24236f98b
Deepika Pal <117921277+1606DP@users.noreply.github.com>
2026-07-17 12:59:00 +0530

    Update Education.js

--- client/src/pages/Home/Education.js
+++ client/src/pages/Home/Education.js
@@ -1,48 +1,43 @@
-import React from "react";
-import SectionTitle from "../../components/SectionTitle";
-import { useSelector } from "react-redux";
+import React from 'react'
+import SectionTitle from '../../components/SectionTitle'
+import { useSelector } from 'react-redux';
 
-function About() {
-   const { loading, portfolioData } = useSelector((state) => state.root);
-  const { about } = portfolioData;
-  const { skills , lottieURL , description1 , description2 } = about;
+function Education() {
+  const [selectedItemIndex, setSelectedItemIndex] = React.useState(0);
+  const { portfolioData } = useSelector((state) => state.root);
+  const { education } = portfolioData;
   return (
-
     <div>
-      <SectionTitle title="About" />
-      <div className="flex w-full items-center sm:flex-col">
-        <div className="h-[70vh] w-full sm:w-full">
-          <dotlottie-wc
-            src={lottieURL}
-            background="transparent"
-            speed="1"
-            autoplay
-            className="w-full h-full"
-          ></dotlottie-wc>
+      <SectionTitle title="Education"/>
+      <div className="flex py-10 gap-20 sm:flex-col">
+        <div className="flex flex-col gap-10 border-l-2 border-[#135e4c82] w-1/3 sm:flex-row sm:overflow-x-scroll sm:w-full">
+          {education.map(
+          (education,index) => (
+            <div onClick={() => {
+              setSelectedItemIndex(index);
+            }}
+            className="cursor-pointer">
+              <h1 className={`text-xl px-5 ${selectedItemIndex === index ? "text-tertiary border-tertiary border-l-4 -ml-[3px] bg-[#50dba826] py-3 " : "text-white border-x-tertiary"}`}>
+                {education.period}
+                </h1>
+              </div>
+          )
+        )}
         </div>
-        <div className="flex flex-col gap-5 w-1/2 sm:w-full">
-          <p className="text-white">
-            {description1 || ''}
-          </p>
-          <p className="text-white">
-           {description2 || ''}
-          </p>
+        <div className="flex flex-col gap-10">
+          <h1 className="text-secondary text-xl">
+            {education[selectedItemIndex].school}
+          </h1>
+          <h1 className="text-white text-xl">
+            {education[selectedItemIndex].title}
+          </h1>
+          <h1 className="text-tertiary text-xl">
+            {education[selectedItemIndex].description}
+          </h1>
         </div>
-      </div>
-      < div className="py-5">
-        <h1 className="text-tertiary text-xl">Here are a few technologies I've been working with recently:</h1>
-       <div className="flex flex-wrap gap-10 mt-5">
-        {
-        skills.map((skills,index) => (
-        <div className='border border-tertiary py-3 px-10'>
-          <h1 className="text-tertiary">{skills}</h1>
-        </div>
-        ))
-       }
-       </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default About;
+export default Education;
