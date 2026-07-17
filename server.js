5f4c9fb7f6f421fc24ff3e0e725230c5ee505e1f
Deepika Pal <117921277+1606DP@users.noreply.github.com>
2026-07-17 10:58:05 +0530

    Add CORS support for specific origin

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const portfolioRoute = require("./routes/portfolioRoute");
 app.use(express.json());
 
 app.use("/api/portfolio", portfolioRoute);
+const cors = require("cors");
+
+app.use(cors({
+  origin: "https://portfolio-1-l56w.onrender.com",
+  credentials: true
+}));
 
 const port = process.env.PORT || 5000;
 
