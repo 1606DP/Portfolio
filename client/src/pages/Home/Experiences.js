de44f51cad2165bcd4ae49e89936495a175e4969
Deepika Pal <117921277+1606DP@users.noreply.github.com>
2026-07-17 12:57:41 +0530

    Update Experiences.js

--- client/src/pages/Home/Experiences.js
+++ client/src/pages/Home/Experiences.js
@@ -5,8 +5,7 @@ import { useSelector } from "react-redux";
 function Experiences() {
   const [selectedItemIndex, setSelectedItemIndex] = React.useState(0);
   const { portfolioData } = useSelector((state) => state.root);
-  const experiences = portfolioData?.experiences || [];
-  const selectedExperience = experiences[selectedItemIndex] || {};
+  const { experiences } = portfolioData;
 
   return (
     <div>
@@ -31,13 +30,13 @@ function Experiences() {
         </div>
         <div className="flex flex-col gap-10">
           <h1 className="text-secondary text-xl">
-            {selectedExperience.title || ""}
+            {experiences[selectedItemIndex].title}
           </h1>
           <h1 className="text-tertiary text-xl">
-            {selectedExperience.company || ""}
+            {experiences[selectedItemIndex].company}
           </h1>
           <p className="text-white">
-           {selectedExperience.description || ""}
+           {experiences[selectedItemIndex].description}
           </p>
         </div>
       </div>
