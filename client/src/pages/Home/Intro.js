f6e4616f252d0babd9b8b126181e2851370723e7
Deepika Pal <117921277+1606DP@users.noreply.github.com>
2026-07-18 21:53:36 +0530

    Update Intro.js

--- client/src/pages/Home/Intro.js
+++ client/src/pages/Home/Intro.js
@@ -3,12 +3,13 @@ import { useSelector } from "react-redux";
 
 function Intro() {
   const { loading, portfolioData } = useSelector((state) => state.root);
-  const { intro } = portfolioData;
+  const intro = portfolioData?.intro || {};
   const { firstName, lastName, welcomeText, description, caption } = intro;
+
   return (
     <div className="h-[80vh] bg-primary flex flex-col items-start justify-center gap-8 py-10 ">
       <h1 className="text-white">{welcomeText || ""}</h1>
-      <h1 className="text-6xl sm:text-3xl text-secondary font semibold">
+      <h1 className="text-6xl sm:text-3xl text-secondary font-semibold">
         {firstName || ""} {lastName || ""}
       </h1>
       <h1 className="text-6xl sm:text-3xl text-white font-semibold">
